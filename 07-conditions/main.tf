bcd365e7ff73d574723928c1f4267b11d97dc6f1
admin <suhasy983@gmail.com>
2024-08-17 00:28:14 +0530

    for each loop example

---
+++ 07-conditions/main.tf
@@ -0,0 +1,13 @@
+#condition in TF is to pick a value , not about running block
+# = expression ? TRUE_VAL : FALSE_VAL
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z02540113N1Z961N7HMWN"
+  name    = "test.dev.sydevops80.online"
+  type    = "A"
+  ttl     = var.ttl == ? 15 : var.ttl
+  records = ["1.1.1.1"]
+}
+
+variable "ttl" {}
+
