ccaa9321d890fdd9d211bc47e8440ea4f40b6cbc
admin <suhasy983@gmail.com>
2024-08-16 20:04:44 +0530

    count and function terraform

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "dns_record" {
   count = length(var.component)
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "${var.component[count.index]}.${var.domain_name}"
+  name    = "${var.component[count.index]}.dev.${var.domain_name}"
     type    = "A"
   ttl     = 15
   records = [aws_instance.instance[count.index].private_ip]
