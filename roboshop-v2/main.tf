f7a2d77c908df350c2d98846387936da87d74949
admin <suhasy983@gmail.com>
2024-08-16 11:15:53 +0530

    variables output practice

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,22 +1,3 @@
-variable "ami" {
-  default = "ami-041e2ea9402c46c32"
-}
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-variable "vpc_security_group_ids" {
-  default = ["sg-0af0abf0c61856bbd"]
-}
-
-variable "zone_id" {
-  default = "Z02540113N1Z961N7HMWN"
-}
-
-variable "domain_name" {
-  default = "sydevops80.online"
-}
-
 resource "aws_instance" "frontend" {
   ami           = var.ami
   instance_type = var.instance_type
@@ -47,7 +28,7 @@ resource "aws_instance" "mongo" {
 
 resource "aws_route53_record" "mongo" {
   zone_id = var.zone_id
-  name    = "frontend.dev.${var.domain_name}"
+  name    = "mongo.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.mongo.private_ip]
@@ -64,7 +45,7 @@ resource "aws_instance" "catalouge" {
 
 resource "aws_route53_record" "catalouge" {
   zone_id = var.zone_id
-  name    = "frontend.dev.${var.domain_name}"
+  name    = "catalouge.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.catalouge.private_ip]
---
+++ roboshop-v2/variables.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+  default = "ami-041e2ea9402c46c32"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-0af0abf0c61856bbd"]
+}
+
+variable "zone_id" {
+  default = "Z02540113N1Z961N7HMWN"
+}
+
+variable "domain_name" {
+  default = "sydevops80.online"
+}
