313ba3a72f9a6ee4905ebd207b44a7ff4b5b776b
admin <suhasy983@gmail.com>
2024-08-17 00:32:17 +0530

    for each loop example

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -5,7 +5,7 @@ resource "aws_route53_record" "frontend" {
   zone_id = "Z02540113N1Z961N7HMWN"
   name    = "test.dev.sydevops80.online"
   type    = "A"
-  ttl     = var.ttl == ? 15 : var.ttl
+  ttl     = var.ttl  == "" ? 15 : var.ttl
   records = ["1.1.1.1"]
 }
 
