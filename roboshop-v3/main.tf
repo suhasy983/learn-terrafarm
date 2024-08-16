53def2054310ba641421aaba376bfc12b6a9d386
admin <suhasy983@gmail.com>
2024-08-16 20:01:23 +0530

    count and function terraform

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -9,14 +9,14 @@ resource "aws_instance" "instance" {
   }
 }
 
-# resource "aws_route53_record" "dns_record" {
-#   count = length(var.component)
-#   zone_id = data.aws_route53_zone.zone.zone_id
-#   name    = "${var.component[count.index]}.${var.domain_name}"
-#     type    = "A"
-#   ttl     = 15
-#   records = [aws_instance.frontend.private_ip]
-# }
+resource "aws_route53_record" "dns_record" {
+  count = length(var.component)
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.component[count.index]}.${var.domain_name}"
+    type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance[count.index].private_ip]
+}
 
 
 
