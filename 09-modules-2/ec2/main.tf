9eaaf8d996e8f2f178bf5f927c2d4bf06cabf5e2
admin <suhasy983@gmail.com>
2024-08-17 18:37:44 +0530

    for each loop example

---
+++ 09-modules-2/ec2/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "test" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0af0abf0c61856bbd"]
+
+  tags = {
+    Name = "test"
+  }
+}
